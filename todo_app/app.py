fa351c991a59148ecb18baa3e0a19f4fb7ff70b8
ianm1968 <57989417+ianm1968@users.noreply.github.com>
2022-03-25 15:48:51 +0000

    form added

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
-
+from flask import Flask,render_template,request,redirect
 from todo_app.flask_config import Config
+# import todo_app.data.session_items as to_do_session
+from todo_app.data.session_items import get_items,add_item
+
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +10,10 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World! edit in branch'
+    return render_template("index.html", to_do_list=get_items())
+    
+@app.route('/add', methods=['POST'])
+def add_item_by_title():
+    add_item(request.form.get('task_title'))
+    return redirect('/')
+
