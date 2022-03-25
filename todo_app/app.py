2bcd31bfe90ab5db18a3a3511ec2df99c234b727
ianm1968 <57989417+ianm1968@users.noreply.github.com>
2022-03-25 15:51:21 +0000

    errr thought it was puished

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,9 @@
 from flask import Flask,render_template,request,redirect
 from todo_app.flask_config import Config
-# import todo_app.data.session_items as to_do_session
 from todo_app.data.session_items import get_items,add_item
-
 
 app = Flask(__name__)
 app.config.from_object(Config())
-
 
 @app.route('/')
 def index():
