120e272b500d765f21038a6732934025f0852796
ianm1968 <57989417+ianm1968@users.noreply.github.com>
2022-04-18 22:22:05 +0100

    Refactor & remove unnecessary global var

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,15 +5,10 @@ from todo_app.data.session_items import get_items,add_item,get_item,save_item,de
 app = Flask(__name__)
 app.config.from_object(Config())
 
-to_do_list=[]
-
 @app.route('/')
 def index():
-    global to_do_list
-    to_do_list = get_items()
-    to_do_list = sorted(to_do_list, key=lambda item: item.get('status'), reverse=True)
-    # csv_mapping_list = sorted(csv_mapping_list, key=lambda item: item.get("Age"))
-    return render_template("index.html", to_do_list=to_do_list)
+    sorted_items = sorted(get_items(), key=lambda item: item.get('status'), reverse=True)
+    return render_template("index.html", to_do_list=sorted_items)
     
 @app.route('/add', methods=['POST'])
 def add_item_by_title():
@@ -22,9 +17,8 @@ def add_item_by_title():
 
 @app.route('/complete', methods=['POST'])
 def complete_item_by_title():
-    global to_do_list
     item_to_complete = request.form.get('task_title')
-    for item in to_do_list:
+    for item in get_items():
         if item['title'] == item_to_complete:
             item['status'] = 'Completed'
             save_item(item)
@@ -33,9 +27,8 @@ def complete_item_by_title():
 
 @app.route('/delete', methods=['POST'])
 def delete_item_by_title():
-    global to_do_list
     item_to_delete = request.form.get('task_title')
-    for item in to_do_list:
+    for item in get_items():
         if item['title'] == item_to_delete:
             delete_item(item['id'])
             break
